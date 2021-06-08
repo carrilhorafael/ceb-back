4603bc4321bd6481c2a121caed38b3df59765022
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-08 20:33:09 -0300

    add validations on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, :email, :password_confirmation, :cpf, :phone, presence: true
+    validates :password, :password_confirmation, length: {minimum: 8}, if: :password
     validates :email, format: {with: /\b[A-Z0-9._%a-z\-]+@+[a-z]+.[a-z]{2,4}\z/, message: "Utilize o seu email da UFF. Exemplo: meuemail@id.uff.br"}
     validates :cpf, format: {with: /\b\d{3}\.\d{3}\.\d{3}-\d{2}\z/, message: "Utilize um cpf valido. Exemplo: 000.000.000-00"}
     validates :phone, format: {with: /(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})/, message: "Utilize um telefone valido. Exemplo: (21) 90000-0000"}
