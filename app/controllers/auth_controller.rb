e813a8ef66ff389b3b4c6e0bffce10bf924713e0
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-08 18:41:01 -0300

    add jsonwebtoken and login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,13 @@
 class AuthController < ApplicationController
     def login
-        
+        @user = User.find_by(email: params[:user][:credential])
+        @user = User.find_by(cpf: params[:user][:credential]) if @user.nil?
+        if @user&.authenticate(params[:user][:password])
+            token = JsonWebToken.encode({user_id: @user.id})
+            render json: {token: token}
+        else
+            render json: {error: "Não foi possível fazer o login"}, status: 401
+        end
     end
     def sign_up
         # @address = Address.new(address_params)
