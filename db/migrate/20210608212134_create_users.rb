92d0e75a2927f4817807b80e0d1305d94ad7cbd3
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-08 18:21:52 -0300

    add model users

---
+++ db/migrate/20210608212134_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+      t.string :email
+      t.string :phone
+      t.string :cpf
+
+      t.timestamps
+    end
+  end
+end
