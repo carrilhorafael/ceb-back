b4b625b4ac98d6b280672ad8ef26bb2f3d99e0b2
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-09 15:04:52 -0300

    add validations on restaurant and modify serializer informations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
-  belongs_to :owner
+  belongs_to :owner, class_name: "User"
+  belongs_to :address
+
+  validates :name, :cnpj, :open_at, :close_at, presence: true
 end
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,9 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :open_at, :close_at
+  attributes :id, :name, :cnpj, :open_at, :close_at, :address_text
   has_one :owner
+
+  def address_text
+    self.object.address.street + ", " + self.object.address.number + ", " + self.object.address.city + ", " + self.object.address.state  
+  end
+
 end
