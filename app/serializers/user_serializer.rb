a2f5ea688164c2bb7e030c508aeccbd19905a4f6
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-09 14:55:06 -0300

    fix user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :phone, :cpf, :has_validated, :role, :times_logged, :last_login, :address_text,
+  attributes :id, :name, :email, :phone, :cpf, :has_validated, :role, :times_logged, :last_login, :address_text
 
   def address_text
     self.object.address.street + ", " + self.object.address.number + ", " + self.object.address.city + ", " + self.object.address.state  
